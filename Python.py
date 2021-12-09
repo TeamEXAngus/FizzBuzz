95fee77e46a8516db716e32a104d99ef69ce2ca2
TeamEXAngus <83906925+TeamEXAngus@users.noreply.github.com>
2021-12-10 01:16:38 +1100

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,7 +1,5 @@
 def FizzBuzz(max):
-
   for i in range(1, max + 1):
-  
     output = ""
     
     if i % 3 == 0:
@@ -14,3 +12,12 @@ def FizzBuzz(max):
       output = str(i)
     
     print(output)
+    
+  
+def FizzBuzz2(max):
+    for i in range(1, Max + 1):
+      print(
+        "Fizz" if (F := i % 3 == 0) else "" +
+        "Buzz" if (B := i % 5 == 0) else "" +
+        str(i) if (not (F or B)) else ""
+      )
