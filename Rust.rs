e1c16160c874088c809b0842dbed1bb0828910d0
TeamEXAngus <83906925+TeamEXAngus@users.noreply.github.com>
2021-12-11 19:28:02 +1100

    Update Rust.rs
    Switched to pattern-matching approach

--- Rust.rs
+++ Rust.rs
@@ -1,27 +1,13 @@
 fn fizz_buzz(max: i32)
 {
-  let mut output;
-
   for i in 1..max
   {
-    output = "".to_string();
-
-    if i % 3 == 0
+    match (i % 3, i % 5) 
     {
-      output += "Fizz";
-    }
-
-    if i % 5 == 0
-    {
-      output += "Buzz";
-    }
-
-    if output == ""
-    {
-      output = i.to_string();
-    }
-
-    println!("{}", output)
-    
+      (0, 0) => println!("FizzBuzz"),
+      (0, _) => println!("Fizz"),
+      (_, 0) => println!("Buzz"),
+      (_, _) => println!("{}", i)
+    };
   }
 }
